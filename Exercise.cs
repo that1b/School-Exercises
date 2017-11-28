71ef56af0fbdb0b36feb51f1736febbe5c02737b
Tolga <31797377+that1b@users.noreply.github.com>
2017-11-28 22:22:49 +0200

    Create Exercise.cs

---
+++ Exercise.cs
@@ -0,0 +1,17 @@
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int[][] jaggedA = new int[2][];
+            jaggedA[0] = new int[3] { 1, 2, 3 };
+            jaggedA[1] = new int[1] { 4 };
+            foreach(var arr in jaggedA)
+            {
+                foreach (var element in arr)
+                    Console.Write("{0}\t", element);
+                Console.WriteLine();
+            }
+        }
+    }
+}

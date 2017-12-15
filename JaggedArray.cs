d80add45b8e3e402163262f1c4fc6b5e54a6def8
Tolga <31797377+that1b@users.noreply.github.com>
2017-12-15 17:46:51 +0200

    Update JaggedArray.cs

--- JaggedArray.cs
+++ JaggedArray.cs
@@ -1,17 +1,17 @@
+using System;
+class JaggedArray
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[][] jaggedA = new int[2][];
+        jaggedA[0] = new int[3] { 1, 2, 3 };
+        jaggedA[1] = new int[1] { 4 };
+        foreach (var arr in jaggedA)
         {
-                int[][] jaggedA = new int[2][];
-            jaggedA[0] = new int[3] { 1, 2, 3 };
-            jaggedA[1] = new int[1] { 4 };
-            foreach(var arr in jaggedA)
-            {
-                foreach (var element in arr)
-                    Console.Write("{0}\t", element);
-                Console.WriteLine();
-            }
+            foreach (var element in arr)
+            Console.Write("{0} ", element);
+            Console.WriteLine();
         }
+        Console.ReadLine();
     }
 }

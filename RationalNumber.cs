f6b9af32b4343988697f7da8cdc0a13494787688
Tolga Hatib <31797377+that1b@users.noreply.github.com>
2018-03-17 17:37:24 +0200

    Update RationalNumber.cs

--- RationalNumber.cs
+++ RationalNumber.cs
@@ -3,12 +3,35 @@ namespace Homework
 {
     class Program
     {
+        class RationalNumber
+        {
+            private int numerator, denominator;
+            public RationalNumber()
+            {
+                numerator = 0;
+                denominator = 1;
+            }
+            public RationalNumber(int num, int denom)
+            {
+                numerator = num;
+                denominator = denom;
+            }
+            public decimal ToDecimal()
+            {
+                return (decimal)numerator / denominator;
+            }
+            public override string ToString()
+            {
+                return string.Format("{0}/{1}", numerator, denominator);
+            }
 
-        static void Main(string[] args)
+        }
+
+            static void Main(string[] args)
         {
             RationalNumber a = new RationalNumber();
             RationalNumber b = new RationalNumber(1, 4);
-            RationalNumber c = new RationalNumber(2,6);
+            RationalNumber c = new RationalNumber(2, 6);
             Console.WriteLine("a={0},decimal:{1}", a, a.ToDecimal());
             Console.WriteLine("b={0},decimal:{1}", b, b.ToDecimal());
             if (c != null)
